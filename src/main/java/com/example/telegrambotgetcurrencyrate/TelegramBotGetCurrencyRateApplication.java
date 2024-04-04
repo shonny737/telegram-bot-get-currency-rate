22bb01e76fcd5b9f17ef85bab65b54a7fe244df7
shonny737 <shonny737@gmail.com>
2024-04-04 12:12:20 +0300

    Initial commit

---
+++ src/main/java/com/example/telegrambotgetcurrencyrate/TelegramBotGetCurrencyRateApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotgetcurrencyrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotGetCurrencyRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotGetCurrencyRateApplication.class, args);
+	}
+
+}
